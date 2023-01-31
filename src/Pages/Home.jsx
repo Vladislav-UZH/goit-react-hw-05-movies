42b75c88b31e61c0af9bbb32e11fe574536d24f4
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-01-31 13:08:12 +0200

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit assumenda
+        at ipsa facere excepturi, blanditiis eius rerum. Autem pariatur, sequi
+        placeat eius repellat animi quos aspernatur quae inventore sed
+        repellendus?
+      </p>
+    </main>
+  );
+};
